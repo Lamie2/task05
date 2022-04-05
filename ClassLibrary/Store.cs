b4bb00b7f5b5a0eab02219e1bf4c50dca6281413
Lamie2 <100703059+Lamie2@users.noreply.github.com>
2022-04-05 10:57:00 +0400

    first commit

---
+++ ClassLibrary/Store.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassLibrary
+{
+    public class Store
+    {
+        Product[] _products=new Product[0];
+        public Product[] Products { get => _products; }
+        public int NotebookLimit { get; set; }
+        public int PhoneLimit { get; set; }
+
+        public void AddProduct(Product product)
+        {
+            foreach (var pr in _products)
+            {
+                if (pr is Notebook)
+                {
+                    Notebook notePr = pr as Notebook;
+                    if (_products.Length < NotebookLimit)
+                    {
+                        Array.Resize(ref _products, _products.Length + 1);
+                        _products[_products.Length - 1] = product;
+                    }
+                }
+                if (pr is Phone)
+                {
+                    Phone phonePr = pr as Phone;
+                    if (_products.Length < PhoneLimit)
+                    {
+                        Array.Resize(ref _products, _products.Length + 1);
+                        _products[_products.Length - 1] = product;
+                    }
+                }
+
+            }
+
+            
+           
+        }
+
+
+        public Product FindByNo(int no)
+        {
+            foreach (var pr in _products)
+            {
+                if (pr.No==no)
+                {
+                    return pr;
+                }
+            }
+            Product product = new Product();
+            return product;
+        }
+
+
+        public double CalcNotebookAvg()
+        {
+            int count = 0;
+            double sum = 0;
+            foreach (var pr in _products)
+            {
+                if (pr is Notebook)
+                {
+                    Notebook notePr = pr as Notebook;
+                    count++;
+                    sum += notePr.Price * (100 - notePr.DiscountPercent) / 100;
+                }              
+            }
+            return sum / count;
+        }
+
+        public double CalcPhoneAvg()
+        {
+            int count = 0;
+            double sum = 0;
+            foreach (var pr in _products)
+            {
+                if (pr is Phone)
+                {
+                    Phone phonePr = pr as Phone;
+                    count++;
+                    sum += phonePr.Price*(100-phonePr.DiscountPercent)/100;
+                }
+            }
+            return sum / count;
+        }
+
+
+    }
+}
---
+++ Task05/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using ClassLibrary;
+namespace Task05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Notebook notebook = new Notebook();
+            notebook.No = 7;
+            notebook.Price = 4000;
+            notebook.DiscountPercent = 15;
+
+            Notebook notebook1 = new Notebook();
+            notebook1.No = 11;
+            notebook1.Price = 3000;
+            notebook1.DiscountPercent = 0;
+
+            Notebook notebook2 = new Notebook();
+            notebook2.No = 11;
+            notebook2.Price = 4500;
+            notebook2.DiscountPercent = 30;
+
+
+            Phone phone = new Phone();
+            phone.No = 1;
+            phone.Price = 1500;
+            phone.DiscountPercent = 20;
+
+
+            Phone phone1 = new Phone();
+            phone1.No = 2;
+            phone1.Price = 2000;
+            phone1.DiscountPercent = 0;
+
+            Product product = new Product();
+            Store store = new Store();
+
+            store = notebook;
+            
+            
+        }
+    }
+}
